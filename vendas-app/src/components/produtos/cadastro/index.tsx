7429a2aa362c020ecca280ccf46cf9a34e70e613
Felipe Daniel <felipedaniels622@gmail.com>
2026-05-10 00:14:38 -0300

    Finalizado manutenção da Atualização

--- vendas-app/src/components/produtos/cadastro/index.tsx
+++ vendas-app/src/components/produtos/cadastro/index.tsx
@@ -2,38 +2,39 @@ import { useState } from 'react';
 import { Layout } from "@/components/layout";
 import { Input } from "@/components/common";
 import { useProdutoService } from '@/app/services';
-import { Produto } from '@/app/models/produtos'
-import { converterBigDecimal } from '@/app/util/money'
-
+import { Produto } from '@/app/models/produtos';
+import { converterBigDecimal } from '@/app/util/money'; 
 
 export const CadastroProdutos: React.FC = () => {
     const service = useProdutoService();
-    const [sku, setSku] = useState('');  // cria uma variavel com valor e tem a variavel e tambem a opção de alterar o valor
+
+    const [sku, setSku] = useState('');
     const [preco, setPreco] = useState('');
     const [nome, setNome] = useState('');
     const [descricao, setDescricao] = useState('');
-    const [id, setId] = useState<string | undefined>('');
+    const [id, setId] = useState<string | undefined>(undefined); 
     const [dataCadastro, setdataCadastro] = useState<string | undefined>('');
 
     const submit = () => {
-        const precoLimpo = preco.replace(/\./g, '').replace(',', '.');
-    
-    const produto: Produto = {
-        id: id ? id : undefined, // Se for "" vira undefined porque é autoincrement
-        sku,
-        preco: parseFloat(precoLimpo) || 0,
-        nome,
-        descricao
-    }
+        const precoConvertido = converterBigDecimal(preco);// tratamento de preço
+        
+        const produto: Produto = {
+            id, 
+            sku,
+            preco: precoConvertido,
+            nome,
+            descricao
+        }
 
-        console.log(produto);
+        console.log("Objeto enviado:", produto);
+
         if (id) {
             service.atualizar(produto)
-                .then(response => console.log(produto));
+                .then(response => console.log("Atualizado!"));
         } else {
             service.cadastrar(produto)
                 .then(produtoResposta => {
-                    setId(produtoResposta.id)
+                    setId(produtoResposta.id);
                     setdataCadastro(produtoResposta.dataCadastro);
                 });
         }
@@ -41,42 +42,45 @@ export const CadastroProdutos: React.FC = () => {
 
     return (
         <Layout titulo="Produtos">
-            {id &&  //só vai incrementar na tela se o id não estiver vazio quando retornar o response
+            
+            {id &&
                 <div className="columns">
-                    <Input label="SKU "
-                        id="inputSku"
-                        value={sku}           
-                        onChange={setSku}      
-                        placeholder='Digite o SKU do produto:' />
+                    <Input label="Código:"
+                        columnClasses="is-half"
+                        value={id}
+                        id="inputId"
+                        disabled />
 
-                    <Input label="Preço "
-                        id="inputPreco"
-                        value={preco}         
-                        onChange={setPreco}    
-                        currency={true}
-                        placeholder='Digite o preço do produto:' />
+                    <Input label="Data de Cadastro:"
+                        columnClasses="is-half"
+                        value={dataCadastro}
+                        id="inputDataCadastro"
+                        disabled />
                 </div>
             }
+
             <div className="columns">
-                <Input label="SKU "
+                <Input label="SKU *"
                     columnClasses="is-half"
                     id="inputSku"
+                    value={sku}           
+                    onChange={setSku}      
                     placeholder='Digite o SKU do produto:' />
-                <Input label="Preço "
+
+                <Input label="Preço *"
                     columnClasses="is-half"
                     id="inputPreco"
-                    placeholder='Digite o preço do produto:'
-                    currency={true} />
-
-
+                    value={preco}         
+                    onChange={setPreco}    
+                    currency={true}
+                    placeholder='Digite o preço do produto:' />
             </div>
 
-
             <div className="columns">
-
-                <Input label="Nome: "
+                <Input label="Nome: *"
                     columnClasses="is-full"
-                    onChange={setNome}
+                    value={nome}          
+                    onChange={setNome}     
                     id="inputNome"
                     placeholder='Digite o nome do produto' />
             </div>
@@ -87,7 +91,8 @@ export const CadastroProdutos: React.FC = () => {
                     <div className="control">
                         <textarea className="textarea"
                             placeholder="Descrição do produto"
-                            id="areaDesc" value={descricao}
+                            id="areaDesc" 
+                            value={descricao}
                             onChange={event => setDescricao(event.target.value)} />
                     </div>
                 </div>
